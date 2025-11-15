23a6148d0b735624bcc06d5a1eab66fdd47a5180
Maksym Rusanov <1maksymrusanov1@gmail.com>
2025-11-15 15:46:16 +0000

    add simple pages as test

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
-import django
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+
 templates = Jinja2Templates(directory="templates")
 
-app = FastAPI()
-@app.get("/")
-async def root():
-        return templates.TemplateResponse("main.html", {'request': request})
 
+app = FastAPI()
+
+
+@app.get("/")
+async def root(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
